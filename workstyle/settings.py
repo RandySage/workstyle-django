49126bdbe2dda3851acb2272c85312e6beb97cb1
venten <venten@781d1ca9-960f-0410-8604-ece4d0c25b9b>
2007-02-26 10:21:32 +0000

    git-svn-id: svn://svn.code.sf.net/p/workstyle-py/code/trunk@6 781d1ca9-960f-0410-8604-ece4d0c25b9b

--- workstyle/settings.py
+++ workstyle/settings.py
@@ -7,12 +7,17 @@ ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
 
+import os
+BASE_DIR = os.path.dirname(__file__)
+FILE_STORE = os.path.join(BASE_DIR, 'stored')
+
+
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'           # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'workstyle'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'root'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'root'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
@@ -20,12 +25,12 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Japan'
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
 SITE_ID = 1
 
@@ -66,6 +71,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'workstyle.urls'
 
 TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -76,4 +82,5 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'workstyle.ws',
 )
---
+++ workstyle/ws/models.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext as _ #_は辞めようという方向だけどさ
+
+
+# Create your models here.
+
+class Status(models.Model):
+    status_id  = models.AutoField(primary_key=True)
+    name       = models.CharField(_('Name'), maxlength=20)
+    sort_order = models.IntegerField(_('Sort Order'), default=0)
+    image      = models.CharField(_('Image Path'), maxlength=256)
+    
+    class Meta:
+        verbose_name = _('Status')
+  
+class TagType(models.Model):
+    tag_type_id  = models.AutoField(primary_key=True)
+    name         = models.CharField(_('Name'), maxlength=20)
+    style_class  = models.CharField(maxlength=10)
+    sort_order   = models.IntegerField(_('Sort Order'), default=0)
+
+    class Meta:
+        verbose_name = _('Tag Type')
+
+class Task(models.Model):
+    task_id              = models.AutoField(primary_key=True)
+    contents             = models.TextField(_('Contents'))
+    estimated_man_hour   = models.FloatField(_('Estimate by man'), max_digits=5, decimal_places=2)
+    tag_list             = models.CharField(maxlength=200, db_index=True)
+    create_date          = models.DateTimeField(_('Create Date'), auto_now_add=True, db_index=True)
+    update_date          = models.DateTimeField(_('Update Date'), auto_now=True, db_index=True)
+    status               = models.ForeignKey(Status, verbose_name=_('Status'))
+    related_task         = models.ManyToManyField('self', verbose_name=_('Task'))
+    
+    def save(self):
+        super(Task, self).save()
+        for tag in tag_list.strip().replace('[', '').split(']'):
+            #処理は冗長だけど…
+            Tag.objects.get_or_create(name__iexact=tag, defaults={'active': True, 'tag_type_id': 1})
+     
+    class Meta:
+         verbose_name=_('Task')
+
+class FileInfo(models.Model):
+    file_id    = models.AutoField(primary_key=True)
+    #ここはoriginalから変更したので注意
+    file       = models.FileField(_('File'), upload_to=settings.FILE_STORE)
+    task       = models.ForeignKey(Task, verbose_name=_('Task'))
+    
+    class Meta:
+        verbose_name=_('File')
+
+class TaskComment(models.Model):
+    comment_id           = models.AutoField(primary_key=True)
+    contents             = models.TextField(_('Comment'))
+    commentator          = models.CharField(_('Commentator'), maxlength=50)
+    update_date          = models.DateTimeField(auto_now=True)
+    task                 = models.ForeignKey(Task)
+    
+    class Meta:
+        verbose_name=_('Comment')
+    
+class Tag(models.Model):
+    tag_id           = models.AutoField(primary_key=True)
+    name             = models.CharField(_('Name'), maxlength=50)
+    active           = models.BooleanField(_('Active'), default=True)
+    tag_type         = models.ForeignKey(TagType)
+
+    class Meta:
+        verbose_name=_('Tag')
