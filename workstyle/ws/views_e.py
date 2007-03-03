e6f1f3943b1c5ad52e6068a0ea952490485fb296
k_37to <k_37to@781d1ca9-960f-0410-8604-ece4d0c25b9b>
2007-03-03 06:49:43 +0000

    git-svn-id: svn://svn.code.sf.net/p/workstyle-py/code/trunk@33 781d1ca9-960f-0410-8604-ece4d0c25b9b

---
+++ workstyle/ws/views_e.py
@@ -0,0 +1,24 @@
+﻿from workstyle.ws.models import TaskComment
+from django.shortcuts import render_to_response
+
+def insert_comment(request,object_id):
+    message = None
+    taskcomment = None
+    try:
+        taskcomment = TaskComment()
+    except TaskComment.DoesNotExist:
+        message = '対象のタスクが見つかりません'
+    else:
+        comment = request.POST.get('comment', None)
+        commentator = request.POST.get('commentator', None)
+        if not comment:
+            message = comment
+        else :
+            taskcomment.comment_id = None
+            taskcomment.contents = comment
+            taskcomment.commentator = commentator
+            taskcomment.task_id = object_id
+            taskcomment.save()
+    res = render_to_response('ws/xml/task.xml', {'task': taskcomment, 'message': message, 'mimetype': 'text/xml'})
+    res['Content-Type'] = 'text/xml; charset=utf-8'
+    return res
