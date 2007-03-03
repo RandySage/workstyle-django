327e316df9211c823fc0aafdcdec1d1d69b87668
k_37to <k_37to@781d1ca9-960f-0410-8604-ece4d0c25b9b>
2007-03-03 07:01:48 +0000

    コメント追加時のアクション追加
    git-svn-id: svn://svn.code.sf.net/p/workstyle-py/code/trunk@34 781d1ca9-960f-0410-8604-ece4d0c25b9b

--- workstyle/urls.py
+++ workstyle/urls.py
@@ -11,10 +11,10 @@ urlpatterns = patterns('',
     (r'^task/add/$', 'django.views.generic.create_update.create_object',{'model': Task}),
     (r'^task/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail',info_dict),
     (r'^task/(?P<object_id>\d+)/contents/update/$', 'workstyle.ws.views.update_contents'),
+    (r'^task/(?P<object_id>\d+)/comment/add/$', 'workstyle.ws.views.insert_comment' ),
     (r'^task/(?P<object_id>\d+)/priority/update/$', 'workstyle.ws.views.update_priority'),
-    (r'^task/(?P<object_id>\d+)/tag/update/$', 'workstyle.ws.views.update_tag'),
     (r'^static/resources/local.js$', 'django.views.generic.simple.direct_to_template',{'template': 'local.js'}),
-    (r'^tag/$', 'django.views.generic.list_detail.object_list',{'queryset': Tag.objects.all().order_by("name"),'allow_empty': True}),
+    (r'^tag/$', 'django.views.generic.list_detail.object_list',{'queryset': Tag.objects.all().order_by("create_date"),'allow_empty': True}),
     (r'^task/(?P<task_id>\d+)/property/update/$','workstyle.ws.views.update_task_property'),
 )
 
